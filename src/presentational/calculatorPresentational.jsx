800d6abd42d6680c4f6e2c6c54887a814c6331ed
Brian-kipkoech-mutai <kbrianmutai@gmail.com>
2024-05-09 12:23:28 +0300

    updated the ui to be more modern

--- src/presentational/calculatorPresentational.jsx
+++ src/presentational/calculatorPresentational.jsx
@@ -43,70 +43,32 @@ const CalculatorUi=({handleAcClick,handleAssignmentClick,handleNumberClick,handl
                </div>
 
             </div>
-            <div className="buttonBox">
-            <div className="mainDiv1">
-
-            <div className="buttons" style={{width:'142px', backgroundColor:'rgb(226, 59, 59)'}}
             
-            onClick={()=>handleAcClick([])}
-            id="clear">ac</div>
+           
+    <div class="buttons-view">
 
-            <div className="buttons"  onClick={()=>handleAssignmentClick('/')}id="divide">/</div>
-            <div className="buttons" onClick={()=>handleAssignmentClick('*')} id="multiply">x</div>
+       <div class="" onClick={()=>handleAcClick([])} id="clear">ac</div>
+       <div class="assignment"  onClick={()=>handleAssignmentClick('/')} id="divide">/</div>
+       <div class="assignment" onClick={()=>handleAssignmentClick('*')} id="multiply">x</div>
+       <div class="" onClick={()=>handleNumberClick(7)} id="seven">7</div>
+       <div class="" onClick={()=>handleNumberClick(8)}id="eight">8</div>
+       <div class="" onClick={()=>handleNumberClick(9)}id="nine">9</div>
+       <div class="assignment" onClick={()=>handleAssignmentClick('-')} id="subtract">-</div>
+       <div class="" onClick={()=>handleNumberClick(4)} id="four">4</div>
+       <div class=""onClick={()=>handleNumberClick(5)} id="five">5</div>
+       <div class="" onClick={()=>handleNumberClick(6)} id="six">6</div>
+       <div class="assignment" onClick={()=>handleAssignmentClick('+')}  id="add">+</div>
+       <div class="" onClick={()=>handleNumberClick(1)} id="one">1</div>
+       <div class="" onClick={()=>handleNumberClick(2)} id="two">2</div>
+       <div class="" onClick={()=>handleNumberClick(3)} id="three">3</div>
+       <div class="assignment" onClick={()=>handleEqualClick(joined)} id="equals">=</div>
+       <div class="" onClick={()=>handleNumberClick(0)} id="zero">0</div>
+       <div class="" onClick={()=>handleNumberClick('.')} id="decimal">.</div>
+       
+    </div>
 
-           </div>
-
-           <div className="mainDiv2">
-
-           <div className="numbers" onClick={()=>handleNumberClick(7)}id="seven">7</div>
-            <div  className="numbers" onClick={()=>handleNumberClick(8)}id="eight">8</div>
-            <div  className="numbers" onClick={()=>handleNumberClick(9)}id="nine">9</div>
-            <div className="buttons" onClick={()=>handleAssignmentClick('-')} id="subtract">-</div>
-
-
-           </div>
-
-           <div className="mainDiv3">
-
-           <div  className="numbers" style={{borderBottom:"none"}}  onClick={()=>handleNumberClick(4)}id="four">4</div>
-            <div className="numbers" onClick={()=>handleNumberClick(5)} id="five">5</div>
-            <div  className="numbers" onClick={()=>handleNumberClick(6)}id="six">6</div>
-            <div className="buttons" onClick={()=>handleAssignmentClick('+')}  id="add">+</div>
-
-           </div>
-
-           <div className="mainDiv4">
-
-           <div className="numbers">
-            <div  className="numbers" style= {{height:"50%" ,borderTopStyle:"none" , borderBottomStyle:"none", borderLeft:"none"} }
-             
-             onClick={()=>handleNumberClick(1)}
-             id="one">1</div>
-            <div  className="numbers" style= {{height:"50%" ,width:'142PX',borderLeft:"none"}}
-            onClick={()=>handleNumberClick(0)}
-            id="zero">0</div>
-           </div>
-            <div  className="numbers" style={{height:"49%"  }}
-            
-            onClick={()=>handleNumberClick(2)}
-            id="two">2</div>
-            <div className="numbers">
-                 <div  className="numbers" style={{height:"50%" ,borderTopStyle:"none", borderLeft:"none"}}
-                 onClick={()=>handleNumberClick(3)}
-                 id="three">3</div>
-                <div  className="numbers" style={{height:"50%" ,borderTopStyle:"none", borderLeft:"none"}}
-                
-                onClick={()=>handleNumberClick('.')}
-                id="decimal">.</div>
-            </div>
-            <div className="buttons" style={{backgroundColor:'rgb(78, 78, 235)'}}
-            
-            onClick={()=>handleEqualClick(joined)}
-            id="equals">=</div>
-
-           </div>
-
-            </div>
+          
+         
 
           </div>
           <div  className="developer"> Design and coded by <a href="https://www.linkedin.com/in/brian-kipkoech-71b5b9248?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B9nnAF3gTQq2oeiliYkWdyA%3D%3D">Brian kipkoech</a></div>
