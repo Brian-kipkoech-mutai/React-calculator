e1d99d3327c73ac30a172ab9b915e702f875be01
Brian-kipkoech-mutai <kbrianmutai@gmail.com>
2024-05-09 16:16:30 +0300

    responsive design

--- src/container/CalculatorComponent.js
+++ src/container/CalculatorComponent.js
@@ -3,11 +3,17 @@ import CalculatorUi from "../presentational/calculatorPresentational"
 import clearAction from "../redux/action/clearAction";
 import evaluateAction from "../redux/action/evaluateAction";
 import originalInputAction from "../redux/action/originalinputAction";
+import clickSound  from "../assets/minimal-pop-click-ui-1-198301.mp3"
 
-
+const audio = new Audio()
+    audio.src=clickSound;
+     const buttonClickSound=()=>{
+        audio.currentTime=0;
+        audio.play()
+     }
    
 const CaculatorComponent=( {handleAcClick,handleAssignmentClick,handleNumberClick,handleEqualClick,evaluated,originalInput})=>{
-
+  
      return(
     <CalculatorUi
     handleAcClick={handleAcClick}
@@ -36,19 +42,27 @@ const CaculatorComponent=( {handleAcClick,handleAssignmentClick,handleNumberClic
   const mapDispatchToProps=(dispatch)=>(
      {
           handleAcClick:(data)=>{
-              dispatch(clearAction(data)) 
+              dispatch(clearAction(data))
+              buttonClickSound()
+               
           },
           handleAssignmentClick:(data)=>{ 
           
                dispatch(originalInputAction(data))
+              buttonClickSound()
+
           },
 
           handleEqualClick:(data)=>{
                dispatch(evaluateAction(data))
+              buttonClickSound()
+
           },
           handleNumberClick:(data)=>{
           
                dispatch(originalInputAction(data))
+              buttonClickSound()
+
 
           }
      }
